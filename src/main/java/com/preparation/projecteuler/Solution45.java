d940a9d0591380777fdeb2303131389881effd4e
Alex <alexthompson1991@hotmail.com>
2017-03-28 11:50:56 -0700

    Finished Solution 45 problem.

--- src/main/java/com/preparation/projecteuler/Solution45.java
+++ src/main/java/com/preparation/projecteuler/Solution45.java
@@ -8,43 +8,50 @@ import java.util.List;
  */
 public class Solution45 extends AbstractSolution {
 
-    private Integer generationLength;
-
-    private List<Integer> triangleList;
-    private List<Integer> allThreeList;
+    private long n;
+    private List<Long> allThreeList;
 
     public Solution45() {
         this.solutionNumber = 45;
-        triangleList = new LinkedList<>();
         allThreeList = new LinkedList<>();
 
-        this.generationLength = 10000;
+        this.n = 10000;
     }
 
-    public void setN(Integer n){
-        this.generationLength = n;
+    public void setN(Long n) {
+        this.n = n;
     }
 
-    private Integer triangleFunction(int n) {return (n * (n + 1)) / 2;}
-
-    public boolean isPentagon(double n) {
-
+    private boolean isTriangle(Long n) {
+        return (-1 + Math.sqrt(1 + 8 * n)) % 2 == 0;
     }
 
-    private boolean isHexagon(double n) {return ( (double) n * ( 2.0 * (double) n - 1)) % 1 == 0;}
+    public boolean isHexagon(Long checkVal) {
+        return (1 + Math.sqrt(1 + 8 * (double) checkVal)) % 4 == 0;
+    }
 
-    private void generateSeries(int n) {
-        for (int i = 0; i < n; i++)
-            triangleList.add(triangleFunction(i));
+    public boolean isPentagon(Long checkVal) {
+        return (1 + Math.sqrt(1 + 24 * (double) checkVal)) % 6 == 0;
+    }
+
+    public boolean allTests(Long n) {
+        return isTriangle(n) & isPentagon(n) & isHexagon(n);
+    }
+
+    public Long triangleCalculation(Long i) {
+        return (i * (i + 1)) / 2;
     }
 
     public void solve() {
-        generateSeries(generationLength);
-        for (int i = 0; i < generationLength - 1; i++) {
-            Integer checkTriangle = triangleList.get(i);
-            if(isPentagon(checkTriangle) & isHexagon(checkTriangle))
-                allThreeList.add(checkTriangle);
+        Long i = new Long(1);
+        Long k = new Long(0);
+        while (k < n) {
+            if (allTests(k))
+                allThreeList.add(k);
+            k = triangleCalculation(i);
+            i++;
         }
-        System.out.println(allThreeList);
+        this.solution = allThreeList.get(allThreeList.size() - 1);
     }
+
 }
